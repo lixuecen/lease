79a14aec593dfa09f5fc0fda9939e68b8d8db666
Xuecen <Xuecen@atguigu.com>
2025-06-23 18:40:19 +0800

    租赁管理相关接口的完成

--- web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
+++ web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
@@ -2,12 +2,14 @@ package com.atguigu.lease;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author 李雪岑
  * @version 1.0
  */
 @SpringBootApplication
+@EnableScheduling
 public class AdminWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminWebApplication.class, args);
