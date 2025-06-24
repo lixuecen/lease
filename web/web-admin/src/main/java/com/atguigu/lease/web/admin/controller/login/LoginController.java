a39775d390cd30607d1b1b276e44f8ced4218fa9
Xuecen <Xuecen@atguigu.com>
2025-06-24 17:15:06 +0800

    用户登录接口的实现

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/login/LoginController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/login/LoginController.java
@@ -1,12 +1,18 @@
 package com.atguigu.lease.web.admin.controller.login;
 
 
+import com.atguigu.lease.common.login.LoginUser;
+import com.atguigu.lease.common.login.LoginUserHolder;
 import com.atguigu.lease.common.result.Result;
+import com.atguigu.lease.common.utils.JwtUtil;
+import com.atguigu.lease.web.admin.service.LoginService;
 import com.atguigu.lease.web.admin.vo.login.CaptchaVo;
 import com.atguigu.lease.web.admin.vo.login.LoginVo;
 import com.atguigu.lease.web.admin.vo.system.user.SystemUserInfoVo;
+import io.jsonwebtoken.Claims;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "后台管理系统登录管理")
@@ -14,21 +20,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin")
 public class LoginController {
 
+    @Autowired
+    private LoginService loginService;
+
     @Operation(summary = "获取图形验证码")
     @GetMapping("login/captcha")
     public Result<CaptchaVo> getCaptcha() {
-        return Result.ok();
+        CaptchaVo captchaVo = loginService.getCaptcha();
+        return Result.ok(captchaVo);
     }
 
     @Operation(summary = "登录")
     @PostMapping("login")
     public Result<String> login(@RequestBody LoginVo loginVo) {
-        return Result.ok();
+        String jwt = loginService.login(loginVo);
+        return Result.ok(jwt);
     }
 
     @Operation(summary = "获取登陆用户个人信息")
     @GetMapping("info")
     public Result<SystemUserInfoVo> info() {
-        return Result.ok();
+        LoginUser loginUser = LoginUserHolder.getLoginUser();
+        Long userId = loginUser.getUserId();
+        SystemUserInfoVo systemUserInfoVo = loginService.getLoginUserInfoById(userId);
+        return Result.ok(systemUserInfoVo);
     }
 }
